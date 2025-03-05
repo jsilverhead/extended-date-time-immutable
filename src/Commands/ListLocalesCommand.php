ae3579866314833b659e896c389611daf0a4a89a
jsilverhand <worldsoeffective@gmail.com>
2025-03-05 09:46:40 +0300

    feat: added listLocales command

---
+++ src/Commands/ListLocalesCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListLocalesCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName("app:list-locales");
+        $this->setDescription("List all locales");
+    }
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $path = getcwd() . "/public/locale";
+
+        $localeFiles = glob($path . "/*.json");
+
+        $resultTable = new Table($output);
+        $resultTable->setHeaders(["#", "Locale"]);
+
+        foreach ($localeFiles as $key => $localeFile) {
+            $localeFile = basename($localeFile);
+            $localeFile = str_replace(".json", "", $localeFile);
+            $resultTable->addRow([++$key, $localeFile]);
+        }
+
+        $resultTable->render();
+
+        return Command::SUCCESS;
+    }
+}
